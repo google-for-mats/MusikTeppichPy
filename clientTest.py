80a0ccef2bf39ac4532fa7889dd006076dce1ba6
MichelIntro <allemannm.intro@gmail.com>
2022-04-11 15:42:33 +0200

    TCP-Connection

---
+++ clientTest.py
@@ -0,0 +1,204 @@
+import socket
+
+host = "192.168.178.154"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s2 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s3 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s4 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s5 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s6 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s7 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s8 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s9 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s10 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s11 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s12 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s13 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s14 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s15 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s16 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s17 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s18 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s19 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s20 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s21 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s22 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s23 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s24 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s25 = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+port1 = 2020
+port2 = 2021
+port3 = 2022
+port4 = 2023
+port5 = 2024
+port6 = 2025
+port7 = 2026
+port8 = 2027
+port9 = 2028
+port10 = 2029
+port11 = 2030
+port12 = 2031
+port13 = 2032
+port14 = 2033
+port15 = 2034
+port16 = 2035
+port17 = 2036
+port18 = 2037
+port19 = 2038
+port20 = 2039
+port21 = 2040
+port22 = 2041
+port23 = 2042
+port24 = 2043
+port25 = 2044
+
+
+s.connect((host, port1))
+r = "Matskannix"
+s.send(r.encode())
+s.close()
+
+
+s2.connect((host, port2))
+r = ''
+s2.send(r.encode())
+s2.close()
+
+
+s3.connect((host, port3))
+r = ''
+s3.send(r.encode())
+s3.close()
+
+
+s4.connect((host, port4))
+r = ''
+s4.send(r.encode())
+s4.close()
+
+
+s5.connect((host, port5))
+r = ''
+s5.send(r.encode())
+s5.close()
+
+
+s6.connect((host, port6))
+r = ''
+s6.send(r.encode())
+s6.close()
+
+
+s7.connect((host, port7))
+r = ''
+s7.send(r.encode())
+s7.close()
+
+
+s8.connect((host, port8))
+r = ''
+s8.send(r.encode())
+s8.close()
+
+
+s9.connect((host, port9))
+r = ''
+s9.send(r.encode())
+s9.close()
+
+
+s10.connect((host, port10))
+r = ''
+s10.send(r.encode())
+s10.close()
+
+
+s11.connect((host, port11))
+r = ''
+s11.send(r.encode())
+s11.close()
+
+
+s12.connect((host, port12))
+r = ''
+s12.send(r.encode())
+s12.close()
+
+
+s13.connect((host, port13))
+r = ''
+s13.send(r.encode())
+s13.close()
+
+
+s14.connect((host, port14))
+r = ''
+s14.send(r.encode())
+s14.close()
+
+
+s15.connect((host, port15))
+r = ''
+s15.send(r.encode())
+s15.close()
+
+
+s16.connect((host, port16))
+r = ''
+s16.send(r.encode())
+s16.close()
+
+
+s17.connect((host, port17))
+r = ''
+s17.send(r.encode())
+s17.close()
+
+
+s18.connect((host, port18))
+r = ''
+s18.send(r.encode())
+s18.close()
+
+
+s19.connect((host, port19))
+r = ''
+s19.send(r.encode())
+s19.close()
+
+
+s20.connect((host, port20))
+r = ''
+s20.send(r.encode())
+s20.close()
+
+
+s21.connect((host, port21))
+r = ''
+s21.send(r.encode())
+s21.close()
+
+
+s22.connect((host, port22))
+r = ''
+s22.send(r.encode())
+s22.close()
+
+
+s23.connect((host, port23))
+r = ''
+s23.send(r.encode())
+s23.close()
+
+
+s24.connect((host, port24))
+r = ''
+s24.send(r.encode())
+s24.close()
+
+
+s25.connect((host, port25))
+r = ''
+s25.send(r.encode())
+s25.close()
