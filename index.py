4e07b3d964a2942e78e9d56eb67fbf7dc686270c
google-for-mats <groenewegm.intro@gmail.com>
2022-04-13 10:29:42 +0200

    returns Database

--- index.py
+++ index.py
@@ -9,7 +9,7 @@ sZeit = myDB.selectSpielzeit("2022-03-24 16:25:00", "2022-04-12 17:29:00")
 #myDB.deleteSpielzeit(52821)
 jingles = myDB.selectJingle("2022-03-24 16:25:00", "2022-04-12 17:29:00")
 #myDB.deleteJingle(52820)
-dateiname = myDB.selectDateiname(43)
+dateiname = myDB.selectDatei(43)
 
 
 print(minID, maxID)
